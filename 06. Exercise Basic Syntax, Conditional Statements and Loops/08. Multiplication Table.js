8473adaee832440491073a80bea4347cacca09c1
gloriayordanova <raevagloria@gmail.com>
2023-11-16 20:05:12 +0000

    Final Version

---
+++ 06. Exercise Basic Syntax, Conditional Statements and Loops/08. Multiplication Table.js
@@ -0,0 +1,11 @@
+function multiplication(num) {
+
+    for (let i = num; i <= num; i++) {
+        for (let j = 1; j <= 10; j++) {
+            console.log(`${i} X ${j} = ${i * j}`);
+        }
+    }
+
+}
+multiplication(5);
+// multiplication(2);
