305f2bdeec4f8c2840900d705b88755dad05832b
gloriayordanova <raevagloria@gmail.com>
2023-11-16 20:16:10 +0000

    Final Version

---
+++ 08. Data Types and Variables/02. Concatenate Names.js
@@ -0,0 +1,14 @@
+function concatenate(first, last, delimiter){
+    console.log(first + delimiter + last);
+}
+
+
+/*
+function concatenate(first, last, delimiter) {
+    console.log(`${first}${delimiter}${last}`);
+}
+*/
+
+concatenate('John', 'Smith', '->');
+concatenate('Jan', 'White', '<->');
+concatenate('Linda', 'Terry', '=>');
