ff21d8e7aa35df35b2e95d091910f5d5d22b3dbd
gloriayordanova <raevagloria@gmail.com>
2023-11-18 12:48:20 +0000

    Final Version

--- 09. Exercise Data Types and Variables/FROM MORE EXERCISES 06. Chess Board.js
+++ 09. Exercise Data Types and Variables/FROM MORE EXERCISES 06. Chess Board.js
@@ -1,18 +1,16 @@
 function solve(n){
-
     let result = '';
 
     for(let i = 0; i < n; i++){
         let row = '';
         for(let j = 0; j < n; j++){
-            row += '#';
+        row += '#';
         }
         result += row;
         result += '\n';
     }
 
     console.log(result);
-
 }
 
 solve(3);
