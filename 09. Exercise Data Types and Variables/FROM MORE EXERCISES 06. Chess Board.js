03bc11b36b688d523462cfebdd3870c7896de788
gloriayordanova <raevagloria@gmail.com>
2023-11-20 21:44:59 +0000

    Final Version

--- 09. Exercise Data Types and Variables/FROM MORE EXERCISES 06. Chess Board.js
+++ 09. Exercise Data Types and Variables/FROM MORE EXERCISES 06. Chess Board.js
@@ -1,14 +1,23 @@
 function solve(n){
     let result = '';
 
+    result += '<div class="chessboard">\n';
+
+
     for(let i = 0; i < n; i++){
         let row = '';
+        row += '   <div>\n';
         for(let j = 0; j < n; j++){
-        row += '#';
+            let color = (i + j) % 2 ? 'white' : 'black';
+            row += `    <span class="${color}"></span>\n`;
         }
+        row += '   </div>\n';
+
         result += row;
-        result += '\n';
     }
+
+    result += '</div>';
+    
 
     console.log(result);
 }
