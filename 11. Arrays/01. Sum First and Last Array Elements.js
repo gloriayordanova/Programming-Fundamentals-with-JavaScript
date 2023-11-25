c97375440ee26c0d63236d7246c836c05f88d91e
gloriayordanova <raevagloria@gmail.com>
2023-11-25 09:05:43 +0000

    Final Version

---
+++ 11. Arrays/01. Sum First and Last Array Elements.js
@@ -0,0 +1,8 @@
+function solve(nums){
+    console.log(nums[0] + nums[nums.length - 1]);
+}
+
+solve([20, 30, 40]);
+solve([10, 17, 22, 33]);
+solve([11, 58, 69]);
+//solve([5]); in that example will return 10 instead of 5, so be aware of this
