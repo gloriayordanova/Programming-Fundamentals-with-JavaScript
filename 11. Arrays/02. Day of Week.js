15908e15f48954b84baa4450b321a2224317db04
gloriayordanova <raevagloria@gmail.com>
2023-11-25 22:40:05 +0000

    Final Version

---
+++ 11. Arrays/02. Day of Week.js
@@ -0,0 +1,60 @@
+function solve(index) {
+
+    if (index < 1 || index > 7) {
+        console.log('Invalid day!');
+    } else {
+        let days = [
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+            'Sunday'
+        ];
+
+        console.log(days[index - 1]);
+    }
+
+}
+
+solve(3);
+solve(6);
+solve(33);
+solve(-1);
+solve('ksjlas'); //e.g. if we do it that way it will give undefined;
+solve('3.5'); //e.g. if we do it that way it will give undefined;
+
+
+
+/*
+!!!!HERE IS AN EXAMPPLE IF I WOULD LIKE TO SHOW UNDEFINED NUMBERS SHOWING AS 'INVALID NUMBER!'
+
+function solve(index) {
+    let days = [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday'
+    ];
+
+    let result = days[index - 1];
+
+    if (result != undefined) {
+        console.log(result);
+    } else {
+        console.log('Invalid number!');
+    }
+
+}
+
+solve(3);
+solve(6);
+solve(33);
+solve(-1);
+solve('ksjlas'); //e.g. if we do it that way it will give undefined;
+solve('3.5'); //e.g. if we do it that way it will give undefined;
+*/
