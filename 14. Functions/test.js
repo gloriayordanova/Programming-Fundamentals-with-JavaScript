c1aebbede67c1d051aa9d5105334a1a3c0560c80
gloriayordanova <raevagloria@gmail.com>
2024-01-23 08:26:51 +0000

    Final Version

--- 14. Functions/test.js
+++ 14. Functions/test.js
@@ -1,25 +1,28 @@
-function solve(a, b, operator){
-    let result;
+// function solve(a, b, operator){
+//     let add = (a, b) => a + b;
+//     let subtract = (a, b) => a - b;
+//     let multiply = (a, b) => a * b;
+//     let divide = (a, b) => a / b;
 
-    switch(operator){
-        case 'add':
-            result = a + b;
-            break;
-        case 'subtract':
-            result = a - b;
-            break;
-        case 'multiply':
-            result = a * b;
-            break;
-        case 'divide':
-            result = a / b;
-            break;
-    }
+//     let result;
 
-    console.log(result);
-    
-}
-solve(5, 3, 'add');           
-solve(7, 1, 'subtract');      
-solve(5, 10, 'multiply');  
-solve(21, 3, 'divide'); 
+//     switch(operator){
+//         case 'add':
+//             result = add(a, b);
+//             break;
+//         case 'subtract':
+//             result = subtract(a, b);
+//             break;
+//         case 'multiply':
+//             result = multiply(a, b);
+//             break;
+//         case 'divide':
+//             result = divide(a, b);
+//             break;
+//     }
+//     console.log(result);
+// }
+// solve(5, 3, 'add');           
+// solve(7, 1, 'subtract');      
+// solve(5, 10, 'multiply');  
+// solve(21, 3, 'divide');
