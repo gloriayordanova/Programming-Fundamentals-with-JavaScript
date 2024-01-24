e54b23c7164dfa5ab5a60d05210863c4ce8fb26e
gloriayordanova <raevagloria@gmail.com>
2024-01-24 21:57:35 +0000

    Finbal Version

--- 14. Functions/test.js
+++ 14. Functions/test.js
@@ -1,34 +1,19 @@
-function solve(a, b, operator){
-    let operations = [
-        (a, b) => a + b,
-        (a, b) => a - b,
-        (a, b) => a * b,
-        (a, b) => a / b
-    ];
+// function solve(a, b, operator){     //the example below is called Associate Array (we will learn that later in the course)
+//     let operations = {
+//         'add': (a, b) => a + b,
+//         'subtract': (a, b) => a - b,
+//         'multiply': (a, b) => a * b,
+//         'divide': (a, b) => a / b
+//     };
 
-    let operationNames = [
-        'add',
-        'subtract',
-        'multiply',
-        'divide'
-    ];
+//     let operation = operations[operator];
+//     console.log(operation(a, b));
+// }
 
-    let index; 
-
-    for(let i = 0; i < operationNames.length; i++){
-        if(operationNames[i] == operator) {
-            index = i;
-        }
-    }
-
-    let operation = operations[index];
-    console.log(operation(a, b));
-}
-
-solve(5, 3, 'add');           
-solve(7, 1, 'subtract');      
-solve(5, 10, 'multiply');  
-solve(21, 3, 'divide');
+// solve(5, 3, 'add');           
+// solve(7, 1, 'subtract');      
+// solve(5, 10, 'multiply');  
+// solve(21, 3, 'divide');
 
 
-    
+
