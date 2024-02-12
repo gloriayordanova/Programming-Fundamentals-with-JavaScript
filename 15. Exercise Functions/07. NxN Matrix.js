d744ded6573bbcb97866522fdb3a8c2f7a1a77b0
gloriayordanova <raevagloria@gmail.com>
2024-02-12 21:38:24 +0000

    Final Version

--- 15. Exercise Functions/07. NxN Matrix.js
+++ 15. Exercise Functions/07. NxN Matrix.js
@@ -1,12 +1,12 @@
-function nxnMatrix(n){
-    for(let rowNum = 1; rowNum <= n; rowNum++){
-        let curRow = `${n} `.repeat(n); // this `${n}` is another way of convering a Number to String
-        console.log(curRow);
-    }
+// function nxnMatrix(n){
+//     for(let rowNum = 1; rowNum <= n; rowNum++){
+//         let curRow = `${n} `.repeat(n); // this `${n}` is another way of convering a Number to String
+//         console.log(curRow);
+//     }
     
 
-}
+// }
 
-// nxnMatrix(3);
-// nxnMatrix(7);
-// nxnMatrix(2);
+// // nxnMatrix(3);
+// // nxnMatrix(7);
+// // nxnMatrix(2);
