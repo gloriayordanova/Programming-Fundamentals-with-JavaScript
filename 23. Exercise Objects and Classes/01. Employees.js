96aecc0c00dae14dd0bb50ea6da4d7e35712dd6f
gloriayordanova <raevagloria@gmail.com>
2024-04-03 20:35:26 +0100

    Final Version

---
+++ 23. Exercise Objects and Classes/01. Employees.js
@@ -0,0 +1,21 @@
+function employees(arr){
+    for(let name of arr){
+        let employee = {
+            
+        }
+    }
+}
+
+employees([
+    'Silas Butler',
+    'Adnaan Buckley',              
+    'Juan Peterson',
+    'Brendan Villarrea'
+]);
+
+// employees([
+//     'Samuel Jackson',
+//     'Will Smith',
+//     'Bruce Willis',
+//     'Tom Holland'
+// ]);
