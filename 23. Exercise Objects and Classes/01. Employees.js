a2f23b7342a0094072c32d2577f83938e4a6ac72
gloriayordanova <raevagloria@gmail.com>
2024-04-04 21:28:42 +0100

    Final Version

--- 23. Exercise Objects and Classes/01. Employees.js
+++ 23. Exercise Objects and Classes/01. Employees.js
@@ -1,8 +1,32 @@
 function employees(arr){
+    for(let employeeName of arr){
+        let employee = { name: employeeName, personalNum: employeeName.length };  
+        console.log(`Name: ${employee.name} -- Personal Number: ${employee.personalNum}`);
+    }
+}
+
+employees([
+    'Silas Butler',
+    'Adnaan Buckley',              
+    'Juan Peterson',
+    'Brendan Villarrea'
+]);
+
+// employees([
+//     'Samuel Jackson',
+//     'Will Smith',
+//     'Bruce Willis',
+//     'Tom Holland'
+// ]);
+
+
+
+
+//another way of solving it, JS is innovative even if we just type {name, } see example;
+function employees(arr){
     for(let name of arr){
-        let employee = {
-            
-        }
+        let employee = { name, personalNum: name.length };  //or here I can just type let employee = {employeeName, personalNum: employeeName.length };
+        console.log(`Name: ${employee.name} -- Personal Number: ${employee.personalNum}`);
     }
 }
 
