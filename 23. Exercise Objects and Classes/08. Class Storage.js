5400dd5861ad2966f14f21a5c229b0e14692d885
gloriayordanova <raevagloria@gmail.com>
2024-04-11 21:20:18 +0100

    Final Version

--- 23. Exercise Objects and Classes/08. Class Storage.js
+++ 23. Exercise Objects and Classes/08. Class Storage.js
@@ -18,7 +18,7 @@ class Storage {
     }
 }
 
-let productOne = {name: 'Cucamber', price: 1.50, quantity: 15};
+let productOne = {name: 'Cucumber', price: 1.50, quantity: 15};
 let productTwo = {name: 'Tomato', price: 0.90, quantity: 25}; 
 let productThree = {name: 'Bread', price: 1.10, quantity: 8};
 let storage = new Storage(50);
