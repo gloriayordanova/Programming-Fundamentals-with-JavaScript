098c483ddbdc43cccad3a0ac334198c03456260e
gloriayordanova <raevagloria@gmail.com>
2024-04-26 08:48:46 +0100

    Final Version

--- 25. Associative Arrays/hello.js
+++ 25. Associative Arrays/hello.js
@@ -241,3 +241,20 @@ entries.sort((a, b) => {
 //you can also destructure the entries
 entries.sort(([keyA (/* key */), valueA /* value */], [keyB, valueB]) => {
 });
+
+
+
+//1.Array and 2.Object Destructuring
+
+//1.Array Destructuring
+const x = [1, 2, 3, 4, 5];
+const [y, z] = x;
+console.log(y); //1
+console.log(z); //2
+
+//2. Object Destructuring
+obj = { a: 1, b: 2 };
+const { a, b } = obj;
+//is equivalent to:
+// const a = obj.a;
+// const b = obj.b;
