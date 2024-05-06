874d191cf90cfab7d0d976c9e4db1e490550e9bf
gloriayordanova <raevagloria@gmail.com>
2024-05-06 10:35:02 +0100

    Final Version

--- 26. Exercise Associative Arrays/01. Word Tracker.js
+++ 26. Exercise Associative Arrays/01. Word Tracker.js
@@ -1,5 +1,5 @@
 function wordTracker(arr) {
-    let searchedWords = arr.shift(). split(' ');
+    let searchedWords = arr.shift().split(' ');
     // console.log(searchedWords);  //it will print ['this', 'sentence'] //looking like [] because we used split
     let occurrences = {};
 
@@ -13,7 +13,7 @@ function wordTracker(arr) {
         }
     }
 
-    //because we can't sort of itterate when we have object - we can do so by doing that using array
+    //because we can't sort or itterate when we have object - we can do so by doing that using array
 
     let entries = Object.entries(occurrences).sort((a, b) => b[1] - a[1]);  // if we do .sort((a, b) => b - a); we do b - a because they want it sorted in descending order. //but on that case it won't work because a and b in this case are whole arrays (see console log below). In that case we just need the number of occurences e.g. 2 and 3 which are located in index 1
         //if we do => {  
