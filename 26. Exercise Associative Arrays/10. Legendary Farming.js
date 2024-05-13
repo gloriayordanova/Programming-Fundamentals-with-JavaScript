c9c2cac5e5cfec1c11dbda8321f66129fbaea81b
gloriayordanova <raevagloria@gmail.com>
2024-05-13 08:56:08 +0100

    Final Version

--- 26. Exercise Associative Arrays/10. Legendary Farming.js
+++ 26. Exercise Associative Arrays/10. Legendary Farming.js
@@ -50,6 +50,12 @@ function legendaryFarming(str) {
     for(let [material, qty] of keyMaterialsEntries) {
         console.log(`${material}: ${qty}`);
     }
+
+    let junkMaterialsEntries = Object.entries(junkMaterials).sort((a, b) => a[0].localeCompare(b[0]));
+
+    for(let [material, qty] of junkMaterialsEntries) {
+        console.log(`${material}: ${qty}`);
+    }
 }
 
 legendaryFarming('3 Motes 5 stones 5 Shards 6 leathers 255 fragments 7 Shards');
