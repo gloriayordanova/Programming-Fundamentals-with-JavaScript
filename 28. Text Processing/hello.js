51eafe4a2f0f8ea622411a1d8dabd6cc31d74599
gloriayordanova <raevagloria@gmail.com>
2024-05-20 16:46:07 +0100

    Final Version

--- 28. Text Processing/hello.js
+++ 28. Text Processing/hello.js
@@ -43,4 +43,38 @@ console.log(last); //Expected output: 11
 
 
 let str = "hello world hello";
-console.log(str.indexOf('hello', 1)); //това ще рече почни да търсиш от индекс 1, което значи, че ще пропуснем първото h; //whtn printing we will get 12
+console.log(str.indexOf('hello', 1)); //това ще рече почни да търсиш от индекс 1, което значи, че ще пропуснем първото h; //when printing we will get 12
+
+
+
+let str = "hello world hello";
+let lastIndex = 0;
+while(str.indexOf('hello', lastIndex) != - 1) {  //!= - 1 различно от -1, защото знаем, че при -1 това ще хвърли грешка
+    let foundAt = str.indexOf('hello', lastIndex);
+    console.log('found match at', str.indexOf('hello', foundAt));
+    lastIndex = foundAt + 5;  
+}
+
+
+
+//substring(startIndex, endIndex)  //работи по същия начин като slice, разликата е, че това не работи с отрицателни индекси
+let str = "I am JavaScript developer";
+let sub = str.substring(5, 10);  //ако му подадем например 10, 5 ще работи суъщото, защото то авотматично си ги разменя
+console.log(sub); //Expected output: JavaS
+
+
+
+////substring(startIndex, endIndex) more examples
+let str = 'hello world hello';
+console.log(str.substring(11, -6));  //казахме, че с отрицателни символи не работи, а каво ще направи тук по-малкото минус 6 ще мине отпред, което не съществува и после 11, ще принтира накрая hello world
+//сравнение горното ако го направим със slice
+let str = 'hello world hello';
+console.log(str.slice(11, -6)); //няма да върне нищо, зашото то си следва последователността 11,-6 защото започвайки от 11тия (2рото h na hello (отляво андясно) и спирайки 6 назад (ордясно наляво) реално ще прескочим всички символи и ще спре на спейса)
+
+
+
+//replace(search, replacement) // или какво търсим и с какво бихме искали да го заменим //fyi замества само 1 съвпадения, а ако искаме няколко ползваме regex ли да използваме някакъв цикъл
+let text = "Hello, john@softuni.bg, you have been using john@softuni.bg in you registration.";
+let replacedText = text.replace(".bg", ".com");
+console.log(replacedText); 
+//"Hello, john@softuni.com, you have been using john@softuni.bg in you registration.";
