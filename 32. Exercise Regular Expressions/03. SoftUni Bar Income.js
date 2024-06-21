b846009ddd8299958f5dc12b09c205e1ee7270fd
gloriayordanova <raevagloria@gmail.com>
2024-06-21 16:25:53 +0100

    Final Version

--- 32. Exercise Regular Expressions/03. SoftUni Bar Income.js
+++ 32. Exercise Regular Expressions/03. SoftUni Bar Income.js
@@ -5,27 +5,40 @@
 //· Valid price is any real number followed by '$'
 // и също копираме input-а
 
-// %(?<customer>[A-Z][a-z]+)%<(?<product>\w+)>\|(?<count>\d+)\|(?<price>)  // \w means any word characters; \| правим така , защото не искаме знак | за или, а искаме просто да намерим | в input-а
-
 function softuniBarIncome(arr) {
     let income = 0;
-    let pattern = //;
+    let pattern = /%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.0-9]*(?<price>\d+\.?\d*)\$/;  // \w means any word characters; \| правим така , защото не искаме знак | за или, а искаме просто да намерим | в input-а //по условие, може да имаме всички символи без |$%. затова пишем [^|$%.]
 
     let command = arr.shift();
 
     while(command != 'end of shift') {
-        // ...
+        let match = command.match(pattern);
+
+        if(match) {
+            //console.log(match);
+            let { customer, product, count, price } = match.groups; //винаги имената тук трябва да съвпадат с групите в наишя pattern
+            //console.log(customer);
+            //console.log(product);
+            //console.log(count);
+            //console.log(price);
+            let totalPrice = Number(count) * Number(price);
+
+            income += totalPrice;
+            console.log(`${customer}: ${product} - ${totalPrice.toFixed(2)}`);
+        }
 
         command = arr.shift();
     }
+
+    console.log(`Total income: ${income.toFixed(2)}`);
 }
 
-softuniBarIncome([
-    '%George%<Croissant>|2|10.3$',
-    '%Peter%<Gum>|1|1.3$',
-    '%Maria%<Cola>|1|2.4$',
-    'end of shift'
-]);
+// softuniBarIncome([
+//     '%George%<Croissant>|2|10.3$',
+//     '%Peter%<Gum>|1|1.3$',
+//     '%Maria%<Cola>|1|2.4$',
+//     'end of shift'
+// ]);
 
 // softuniBarIncome([
 //     '%InvalidName%<Croissant>|2|10.3$',
